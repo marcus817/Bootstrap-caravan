89d99333688813896cf89fbbca772bb11e124a55
Marcus Santos <marcus817@gmail.com>
2018-03-07 13:48:01 -0300

    Bootstrap-Estudo
    Estudo feito com Bootstrap 4. Para familiarizar com as novidades e
    testar à usabilidade

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$("#btnLogin").click(function() {
+    $("body").css({"filter":"blur(15px)"});
+    console.log("Aqui");
+});
+
+$("#btnLoginReq").click(function(){
+    $("body").css({"filter":"blur(0px)"});
+});
+
